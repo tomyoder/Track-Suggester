73887230e8ff3941ef1aa37c8b94b388775a8d62
tomyoder <motredoy@gmail.com>
2017-02-17 13:44:59 -0800

    more html, css,js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
-  $("form.#tracksuggester").submit(function()
+  $("form.#tracksuggester").submit(function(event){
 event.preventDefault();
-var front/back = $("input:radio[name=front/back]:checked").val();
-var result;
-if (front/back === "front"){
-  result = 
+var frontback = $("input:radio[name=frontback]:checked").val();
+
+if (frontback === "front"){
+  $("#design").show();
 }
+else if(frontback === "back"){
+  $("#function").show();
+}
+
 });
+var cosize = $("cosize").val();
+
+if (cosize === "1"){
+  $("#large").show();
+}
+else if (cosize === "2"){
+  $("#medium").show();
+}
+else if (cosize === "3"){
+  $("#small").show();
+}
+else if (cosize === "4"){
+  $("#freelance").show();
+}
 
 });
