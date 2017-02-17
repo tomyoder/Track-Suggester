80544c344f7093570fa63b2c8461bd8f62a0ff59
tomyoder <motredoy@gmail.com>
2017-02-17 10:36:16 -0800

    setup basic html, css, js.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("form.#tracksuggester").submit(function()
+event.preventDefault();
+var front/back = $("input:radio[name=front/back]:checked").val();
+var result;
+if (front/back === "front"){
+  result = 
+}
+});
+
+});
